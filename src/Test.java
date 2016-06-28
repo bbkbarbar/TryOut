0dfbcb009ec05ed3e476e6f9595c88e4f70c2d14
Barbár <bbk.barbar@gmail.com>
2016-06-28 06:34:46 +0200

    Try to work with XML files

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,26 @@
+import java.util.ArrayList;
 import java.util.Date;
 
+import hu.barbar.util.filehandling.XMLHandler;
+import hu.barbar.util.tasks.DailyRepeatingTask;
+import hu.barbar.util.tasks.RepeatingTask;
 import hu.barbar.util.tasks.SingleTask;
+import hu.barbar.util.tasks.Task;
 
 public class Test {
 	
-	
+	public static Test me = null;
 
 	public static void main(String[] args) {
-		new Test();
+		me = new Test();
+		me.doIt();
 	}
 
-	public Test(){
+	public Test(){}
+
+	private void doIt(){
+		
+		ArrayList<Task> list = new ArrayList<>();
 		
 		SingleTask st = new SingleTask("doThis1", new Date());
 		SingleTask st2 = new SingleTask("doThis2", new Date());
@@ -20,6 +30,18 @@ public class Test {
 		show(st2.toString());
 		show(st3.toString());
 		
+		
+		RepeatingTask rt = new DailyRepeatingTask(12, "daily concent", new Date(), null);
+		
+		
+		list.add(st);
+		list.add(st2);
+		list.add(st3);
+		
+		
+		XMLHandler xml = new XMLHandler();
+		xml.saveToFile(list, "c:\\testSave.xml");
+		/**/
 	}
 	
 	public String cutPrefix(String text, String prefix){
---
+++ src/hu/barbar/util/filehandling/XMLHandler.java
@@ -0,0 +1,33 @@
+package hu.barbar.util.filehandling;
+
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+import hu.barbar.util.tasks.Task;
+
+public class XMLHandler {
+	
+	public XMLHandler() {
+		
+	}
+	
+	public void saveToFile(ArrayList<Task> list, String filename){
+		XMLEncoder encoder;
+		try {
+			encoder = new XMLEncoder(
+			          	new BufferedOutputStream(
+			        		  new FileOutputStream(filename))
+			);
+		
+			encoder.writeObject(list);
+			encoder.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
